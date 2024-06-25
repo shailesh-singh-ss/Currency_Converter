c7467bd94ca34e0a76d7f89a2ba8475b280f5544
Shailesh Singh <shaileshsingh121122@gmail.com>
2024-06-26 00:31:46 +0530

    first commit

---
+++ src/hooks/useCurrencyinfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyinfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        fetch(url)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+    }, [currency])
+    return data
+}
+
+
+export default useCurrencyinfo;
